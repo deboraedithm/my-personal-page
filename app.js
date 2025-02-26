5606e9598726bd7cc778444d54e63451e7231ec5
Débora Edith <deboraedithm@gmail.com>
2025-02-26 17:52:52 -0300

    main commit

--- app.js
+++ app.js
@@ -23,25 +23,3 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 });
     console.log('current class name: '+ className);
-
-    document.addEventListener("DOMContentLoaded", function () {
-        const textElement = document.getElementById("#typing-text"); // Seleciona o h1 correto
-        const text = "Hi, my name is Debs.";
-        let i = 0;
-    
-        function typeEffect() {
-            if (i < text.length) {
-                textElement.textContent += text.charAt(i);
-                i++;
-                setTimeout(typeEffect, 100); // Velocidade da digitação
-            }
-        }
-    
-        if (textElement) {
-            textElement.textContent = ""; // Garante que o texto inicie vazio
-            setTimeout(typeEffect, 500); // Pequeno atraso antes de começar
-        } else {
-            
-            console.error("Elemento 'typing-text' não encontrado!");
-        }
-    });
